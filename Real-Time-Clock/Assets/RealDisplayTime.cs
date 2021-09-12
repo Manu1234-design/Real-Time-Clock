f08db44c5fca1078b087150ee1ef68382dc893e8
DESKTOP-PL3AINM\GOVT ITI WOMENS <peringhatmanu@gmail.com>
2021-09-12 19:03:42 +0530

    Real-Time-Clock

---
+++ Real-Time-Clock/Assets/RealDisplayTime.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RealDisplayTime : MonoBehaviour
+{
+
+    private Text clockText;
+    
+    void Start (){
+        clockText = GetComponent<Text>();
+    }
+    void Update()
+    {
+        System.DateTime time = System.DateTime.Now;
+
+        clockText.text = time.ToString("HH:mm");
+    }
+}
